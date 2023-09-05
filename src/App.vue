8ee3fbeee529fcd29483e8e67ab488aac2d42d79
Naor Almeida <naoralmeidantonio@gmail.com>
2023-09-05 23:15:57 +0100

    Estrutura Básica

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<template>
+  <figure
+    class="w-1/2 text-white mt-32 mx-auto bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800"
+  >
+  
+    <div class="pt-6 md:p-8 text-center space-y-4">
+      <blockquote>
+        <h1 class="text-lg font-bold">
+         PROJECTO SMART SCHOOL FRONT-END <br> (VUE + TAILWINDCSS)
+        </h1>
+      </blockquote>
+      <figcaption class="font-medium">
+        <div class="text-sky-500 dark:text-sky-400">Naor de Almeida e Mauro Paulo</div>
+        <div class="text-slate-700 dark:text-slate-500">
+          Powered By NM Solutions
+        </div>
+      </figcaption>
+    </div>
+  </figure>
+
+</template>
+
+<style>
+</style>
